ac44e35bd4a4df2fae5201f9583efca530196238
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-06-20 11:44:51 -0700

    Upgrade time trail test framework to cover inconsisten results with original.

--- time_trial.rb
+++ time_trial.rb
@@ -9,25 +9,44 @@ class TimeTrial
 	def compare_methods(method,trials,sample_input,version,object=nil)
 		orig_sum = 0
 		opt_sum = 0
+		result = nil
+		result2 = nil
 		if object
 			trials.times do
-				orig_sum += Benchmark.measure { object.send(method.to_sym,*sample_input) }.total
+				orig_sum += Benchmark.measure { 
+					result = object.send(method.to_sym,*sample_input) 
+				}.total
+
 				if @lookfor
-					opt_sum += Benchmark.measure { object.send("#{method}_#{@lookfor}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) }.total
+					opt_sum += Benchmark.measure { 
+						result2 = object.send("#{method}_#{@lookfor}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) 
+					}.total
 				else
-					opt_sum += Benchmark.measure { object.send("#{method}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) }.total
+					opt_sum += Benchmark.measure { 
+						result2 = object.send("#{method}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) 
+					}.total
 				end
 			end
 		else
 			trials.times do 
-				orig_sum += Benchmark.measure { send(method.to_sym,*sample_input) }.total
+
+				orig_sum += Benchmark.measure { 
+					result = send(method.to_sym,*sample_input) 
+				}.total
+
 				if @lookfor
-					opt_sum += Benchmark.measure { send("#{method}_#{@lookfor}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) }.total
+					opt_sum += Benchmark.measure { 
+						result2 = send("#{method}_#{@lookfor}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) 
+					}.total
 				else
-					opt_sum += Benchmark.measure { send("#{method}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) }.total
+					opt_sum += Benchmark.measure { 
+						result2 = send("#{method}#{ version ? '_' : '' }#{version}".to_sym,*sample_input) 
+					}.total
 				end
 			end
 		end
+
+		raise 'inconsistent results' if result != result2
 
 		[orig_sum,opt_sum]
 	end
@@ -61,16 +80,41 @@ class TimeTrial
 			message = "\n"
 			message += " --------------- #{method} report --------------- \n"
 
-
-
 			if options[:test_cases]
 				options[:test_cases].each do |version|
 					message += " --------------- #{method}_#{version} --------------- \n"
-					orig_sum,opt_sum = compare_methods(method,10,sample_input,version,object)
-					original = orig_sum/trials
-					optimized = opt_sum/trials
-
 					message += "Over #{trials} trials:\n"
+					begin
+						orig_sum,opt_sum = compare_methods(method,10,sample_input,version,object)
+						original = orig_sum/trials
+						optimized = opt_sum/trials
+
+						message += "Original: #{original}\n"
+						message += "Optimized: #{optimized}\n"
+						diff = original - optimized
+						message += "Time difference: #{diff}\n"
+						if diff < 0
+							message += "The optimized solution performs worse.\n"
+						elsif diff > 0
+							message += "The optimized solution performs better.\n"
+						else
+							message += "There is no difference in time.\n"
+						end
+
+					rescue Exception => e
+						message += e.message
+						
+					end
+					message += "\n"
+				end
+
+			else
+				orig_sum,opt_sum = compare_methods(method,10,sample_input,nil,object)
+				original = orig_sum/trials
+				optimized = opt_sum/trials
+
+				message += "Over #{trials} trials:\n"
+				begin
 					message += "Original: #{original}\n"
 					message += "Optimized: #{optimized}\n"
 					diff = original - optimized
@@ -82,26 +126,10 @@ class TimeTrial
 					else
 						message += "There is no difference in time.\n"
 					end
-
-					message += "\n"
-				end
-
-			else
-				orig_sum,opt_sum = compare_methods(method,10,sample_input,nil,object)
-				original = orig_sum/trials
-				optimized = opt_sum/trials
-
-				message += "Over #{trials} trials:\n"
-				message += "Original: #{original}\n"
-				message += "Optimized: #{optimized}\n"
-				diff = original - optimized
-				message += "Time difference: #{diff}\n"
-				if diff < 0
-					message += "The optimized solution performs worse.\n"
-				elsif diff > 0
-					message += "The optimized solution performs better.\n"
-				else
-					message += "There is no difference in time.\n"
+					
+				rescue Exception => e
+						message += e.message
+					
 				end
 
 				message += "\n"
